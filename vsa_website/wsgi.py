393ad360a4aaf0b20aee814be027f5499ec02bb7
Long Nguyen <lnguye782@gmail.com>
2025-01-28 01:13:50 -0500

    fix minor error

--- vsa_website/wsgi.py
+++ vsa_website/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for web-student-org-explorer project.
+WSGI config for vsa_website project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web-student-org-explorer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vsa_website.settings')
 
 application = get_wsgi_application()
